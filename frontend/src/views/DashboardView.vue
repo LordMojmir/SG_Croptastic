94a3c883241f18602ca9ac015b70b654aa1ea05b
Lilia Gerber <ger20411@spengergasse.at>
2024-03-21 16:01:15 +0100

    zwischendingsda

---
+++ frontend/src/views/DashboardView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+import CropGrowthComponent from "@/components/CropGrowthComponent.vue";
+</script>
+<template>
+  <CropGrowthComponent></CropGrowthComponent>
+
+</template>
+<style>
+
+</style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+
+  <BoundingBoxMap></BoundingBoxMap>
+  <RouterLink to="/playground">
+    <button class="bg-primary-700 rounded-xl shadow-md px-3 py-2 text-text-100">Go to playground</button>
+  </RouterLink>
+
+</template>
+<script setup lang="ts">
+import BoundingBoxMap from "@/components/BoundingBoxMap.vue";
+</script>
